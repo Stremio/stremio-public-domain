afa569fa99cef85bd3eb4d37b6c08a407b5ad50e
dexter21767-dev <dexter21767@gmail.com>
2023-08-11 15:16:01 +0100

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const meta_include = require('./include/meta.json');
 const streams_include = require('./include/streams.json');
 
 
-return {
+module.exports = {
     include: {
         meta: meta_include,
         stream: streams_include
